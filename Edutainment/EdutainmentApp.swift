2d807fb04acdf6c4a37a8ad108ff06e68368dca4
Пахаев Хусейн <pakhaev01@gmail.com>
2024-02-24 23:52:30 +0300

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Khusain on 24.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
